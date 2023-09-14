e286eefab24a7c0da3ba6df68c0a4f94d277d509
TitanEkanayake <titanekanayake9@gmail.com>
2023-09-15 00:27:28 +0530

    NavbarUpdate

--- src/scence/Navbar.jsx
+++ src/scence/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = ({isTopOfPage,selectedPage,setSelectedPage}) => {
     return (
         <nav className={`${navbarBackground} z-40 w-full fixed top-0 py-6`}>
             <div className="flex items-center justify-between mx-auto w-5/6">
-                <h4 className="font-playfair text-3xl font-bold px-4">TE</h4>
+                <h4 className="font-playfair text-3xl font-bold">TE</h4>
                 {/* {Desktop Nav} */}
                 {isDesktop ? (
                     <div className="flex justify-between gap-16 font-opensans text-sm font-semibold">
@@ -58,23 +58,21 @@ const Navbar = ({isTopOfPage,selectedPage,setSelectedPage}) => {
                         />
                     </div>
                 ) : (
-                    <div>
-                        <button
-                            className="rounded-full bg-red p-2"
-                            onClick={() => setIsMenuToggled(!isMenuToggled)}
-                        >
-                            <img alt="menu-icon" src={menuIcon} />
-                        </button>
-                    </div>
+                    <button
+                        className="rounded-full bg-red p-2"
+                        onClick={() => setIsMenuToggled(!isMenuToggled)}
+                    >
+                        <img alt="menu-icon" src={menuIcon} />
+                    </button>
                 )}
                 {/* {Mobile Menu Popup} */}
                 {!isDesktop && isMenuToggled && (
-                    <div className="fixed right-0 bottom-0 h-full bg-blue w-[300px]">
+                <div className="fixed right-0 bottom-0 h-full bg-blue w-[300px]">
                     {/* CLOSE ICON */}
                     <div className="flex justify-end p-12">
-                      <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+                    <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
                         <img alt="close-icon" src={closeIcon} />
-                      </button>
+                    </button>
                     </div>
         
                     {/* MENU ITEMS */}
